5afeb4b2501ca7adf6587edaf6ba37417d9ccf76
Lucas M. Dentesani <lucas.dentesani@outlook.com>
2021-11-27 13:29:42 -0300

    Fix setup.py
    Add requirements

--- iol_api/__init__.py
+++ iol_api/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Lucas Matias Dentesani"""
 __email__ = 'cucasf@outlook.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from iol_api.client import IOLClient
 from iol_api.ordenes import OrdenCompra, OrdenFCI, OrdenVenta
--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = [ 
+    'aiohttp==3.8.1',
+    'pandas==1.3.4'
+ ]
 
 test_requirements = ['pytest>=3', ]
 
@@ -39,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/cucasf/iol-api',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
